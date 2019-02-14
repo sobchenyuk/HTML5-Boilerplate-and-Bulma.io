3df2b90b159b3df8ee8c954b7b73c7715f02e56a
sobchenyuk <ablehands@ukr.net>
2019-02-14 22:49:49 +0200

    new commit

--- gulpfile.js
+++ gulpfile.js
@@ -10,27 +10,33 @@ const   source = require('vinyl-source-stream');
 const   watch = require('gulp-watch');
 const   sourcemaps = require('gulp-sourcemaps');
 
+const   fileinclude = require('gulp-file-include');
+
 // gulp settings
 const   livereliad = browserSync.create();
 const   reload = livereliad.reload;
 const path = {
     app: {
         styl: './src/stylus/',
-        js: './src/js/'
+        js: './src/js/',
+        html: './src/view/'
     },
     dist: {
         js: './js/',
         css: './css/',
+        html: './*.html'
     }
 };
 
 // name tasks
+const HTML = 'html';
+const FILE_INCLUDE = 'fileinclude';
 const BROWSER_SYNC = 'browser-sync';
 const JAVA_SCRIPT = 'javascript';
 const STYLUS = 'stylus';
 const WATCHER = 'watcher';
 const DEFAULT = 'default';
-const TYPE_FILE = '.js'
+const TYPE_FILE = '.js';
 
 // gulp TASKS
 
@@ -89,10 +95,31 @@ gulp.task(STYLUS, () => {
         .pipe(reload({stream: true}));
 });
 
+gulp.task('html', function () {
+    // Callback mode, useful if any plugin in the pipeline depends on the `end`/`flush` event
+    return watch(`${path.dist.html}`, { ignoreInitial: false })
+        .pipe(reload({stream: true}));
+});
+
+
+// fileinclude
+gulp.task(FILE_INCLUDE, function() {
+    gulp.src([`${path.app.html}*.html`])
+        .pipe(plumber())
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@file'
+        }))
+        .pipe(gulp.dest('./'))
+        .pipe(reload({stream: true}));
+});
+
 // watcher
 gulp.task( WATCHER, () => {
     watch(`${path.app.styl}/**/*.styl`, () => gulp.start(STYLUS));
     watch(`${path.app.js}/**/*.js`, () => gulp.start(JAVA_SCRIPT));
+    watch(`${path.dist.html}`, () => gulp.start(HTML));
+    watch(`${path.app.html}**/*.html`, () => gulp.start(FILE_INCLUDE));
 });
 
 gulp.task(
@@ -102,6 +129,7 @@ gulp.task(
       BROWSER_SYNC,
       STYLUS,
       JAVA_SCRIPT,
+      FILE_INCLUDE
     ]
 );
 
